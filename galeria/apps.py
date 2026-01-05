98ab25547a63bfce42b32557162dc0c42edfacb6
Danielle Modesti <dani.modesti@icarotech.com>
2026-01-05 07:14:17 -0300

    montando formulários django

--- galeria/apps.py
+++ galeria/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class GaleriaConfig(AppConfig):
+    """App de galeria para que pessoas adicionem e visualizem novas imagens do espaço."""
     name = 'galeria'
